b3ad5bc4cfcea80846da86943032fca9b51b6dc2
Beamanator <dabeamanator@gmail.com>
2018-04-26 16:20:31 +0200

    load players dynamically through FB now

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 // short for $(document).ready(...)
 $(() => {
-    const players = plyr.setup('.player', {
-        debug: false, // Output to console
-        controls: [
-            'play',
-            'progress',
-            'current-time',
-            'mute', 
-            'volume'
-        ],
-        hideControls: false // never hide status bottom status bar
+    const FBdb = new FBUtil();
+
+    FBdb.getAllTracks()
+    .then(tracks => {
+        // Create players for each track
+        Object.keys(tracks).forEach(trKey => {
+            const track = tracks[trKey];
+            const html = Utils_GeneratePlayerHTML(track);
+
+            $('div.container').append(html);
+        });
+
+        // add styling & config for each player
+        const players = plyr.setup('.player', {
+            debug: false, // Output to console
+            controls: [
+                'play',
+                'progress',
+                'current-time',
+                'mute', 
+                'volume'
+            ],
+            hideControls: false // never hide status bottom status bar
+        });
     });
 });
