42498fe13cebe43ff06b73b26bc4593357329ed6
Beamanator <dabeamanator@gmail.com>
2018-04-26 14:47:53 +0200

    cleaned unused stuff

--- js/main.js
+++ js/main.js
@@ -1,47 +1,14 @@
-// (function() {
+// short for $(document).ready(...)
 $(() => {
-    // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
-    // e.g. just plyr.setup(); and leave it at that if you have no need for events
-    // var players = plyr.setup({
     const players = plyr.setup('.player', {
-        // Output to console
-        debug: false,
+        debug: false, // Output to console
         controls: [
-            // 'play-large',// don't allow start on video click
             'play',
             'progress',
             'current-time',
             'mute', 
             'volume'
-            // 'captions',  // don't allow captions
-            // 'fullscreen' // don't allow fullscreen
         ],
         hideControls: false // never hide status bottom status bar
-    });
-    const Player = players[0];
-
-    // Play
-    $('.js-play').click((e) => { 
-        Player.play();
-    });
-    
-    // Pause
-    $('.js-pause').click((e) => {
-        Player.pause();
-    });
-    
-    // Stop
-    $('.js-stop').click((e) => {
-        Player.stop();
-    });
-    
-    // Rewind
-    $('.js-rewind').click((e) => {
-        Player.rewind();
-    });
-    
-    // Forward 
-    $('.js-forward').click((e) => {
-        Player.forward();
     });
 });
